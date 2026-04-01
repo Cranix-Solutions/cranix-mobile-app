f0dce1eadfffb7c8d24377e25666f38e285ab7c1
Peter Varkoly <pvarkoly@cephalix.eu>
2026-04-01 08:44:01 +0200

    Fix acl name

--- src/app/shared/cranix-toolbar/cranix-toolbar.component.ts
+++ src/app/shared/cranix-toolbar/cranix-toolbar.component.ts
@@ -64,7 +64,7 @@ export class CranixToolbarComponent  implements OnDestroy {
         this.countTickets();
       }))
     }
-    if(this.authService.isAllowed('cranix.support')){
+    if(this.authService.isAllowed('cephalix.support')){
       this.countResponses();
       interval(30000).pipe(takeWhile(() => this.alive)).subscribe((func => {
         this.countResponses();
